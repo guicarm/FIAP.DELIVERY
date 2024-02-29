8f154d24130b86882aae838889a483af2ad94768
guicarm <rm98874@fiap.com.br>
2024-02-29 08:51:22 -0300

    project creation spring

---
+++ src/main/java/br/com/fiap/fiapdelivery/FiapdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapdeliveryApplication.class, args);
+	}
+
+}
