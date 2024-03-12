8dfa5ead94613db6c45faf478f03f07e43971b68
Erick <erick.kuwahara79@gmail.com>
2024-03-12 12:08:14 -0300

    feat: data persistence

--- src/main/java/br/com/fiap/fiapdelivery/model/Produto.java
+++ src/main/java/br/com/fiap/fiapdelivery/model/Produto.java
@@ -1,19 +1,22 @@
 package br.com.fiap.fiapdelivery.model;
 
-import java.util.Random;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// RECORD - Gera objetos imutáveis
-//TODO List Ingrediente, Blob imagem?
-public record Produto(Long id, String titulo, String descricao, String ingrediente, String imagem, double valorProduto) {
-
-    public Produto(Long id, String titulo, String descricao, String ingrediente, String imagem, double valorProduto){
-        var key = (id == null) ?  Math.abs(new Random().nextLong()) : id;
-        this.id = key;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.ingrediente = ingrediente;
-        this.imagem = imagem;
-        this.valorProduto = valorProduto;
-    }
-   
+@Data
+@Entity
+public class Produto {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String categoria;
+    private String nome;
+    private Number preco;
+    private Number porcentagem_desconto;
+    private String ingredientes;
+    private String imagem;
 }
---
+++ src/main/java/br/com/fiap/fiapdelivery/repository/CardapioRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.fiapdelivery.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.fiapdelivery.model.Produto;
+
+public interface CardapioRepository extends JpaRepository<Produto, Long>{
+
+}
