fd7fea1ab05d3ff1dfdb09a34c8633ad1232c68c
guicarm <RM98874@fiap.com.br>
2024-04-04 19:44:01 -0300

    add: ValidationErrorHandler

---
+++ src/main/java/br/com/fiap/fiapdelivery/validation/ValidationErrorHandler.java
@@ -0,0 +1,32 @@
+package br.com.fiap.fiapdelivery.validation;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.util.List;
+
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ValidationErrorHandler {
+
+    record ValidationError(String campo, String mensagem){
+        public ValidationError(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public List<ValidationError> handle(MethodArgumentNotValidException exception){
+        return exception
+                .getFieldErrors()
+                .stream()
+                .map(ValidationError::new)
+                .toList();
+    }
+    
+}
